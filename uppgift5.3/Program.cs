e5c25356c82a1fc4d5513da5fbaeeac0b271c73d
Admin <Admin@TEB123007>
2023-01-25 12:07:51 +0100

    Add project files.

---
+++ uppgift5.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_5._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] frågor = {"hur gammal är jag", "Hur mår du idag", "vem är värdens bästa fotbollspelare", "vem är leon"};
+            string[] svar = { "12", "bra", "leon", "leon" };
+            bool spela=true;
+
+            while (spela = true) ;
+            {
+                Console.WriteLine();
+                Console.WriteLine("valj ett tal mellan 1-4");
+                int val=int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine(frågor[val - 1]);
+                string val2=Console.ReadLine();
+            }
+            else ("du svarade fel");
+        }
+    }
+}
